d359cf2e5001c6efe67c766362b1a04ba7bd47ea
Toni Rudolf <toni.rudolf@gmail.com>
2023-08-24 15:14:36 +0200

    Symfony 6 support (#18)

--- Tests/Services/Fixtures/MySubscriber.php
+++ Tests/Services/Fixtures/MySubscriber.php
@@ -26,7 +26,7 @@ class MySubscriber implements EventSubscriberInterface
     /**
      * @inheritdoc
      */
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents(): array
     {
         return [
             LifecycleEvents::CREATED => 'onCalled',
